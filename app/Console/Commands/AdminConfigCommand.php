ab2fbe08032e882bfb42cb140c1c3be6297619df
asundust <asundust@qq.com>
2020-09-20 12:52:56 +0800

    config更新

--- app/Console/Commands/AdminConfigCommand.php
+++ app/Console/Commands/AdminConfigCommand.php
@@ -44,14 +44,35 @@ class AdminConfigCommand extends Command
             case 'new':
                 console_info('后台配置开始处理');
                 foreach ($adminConfigData as $key => $v) {
-                    console_info('　　　　当前处理：'.$v['name'].' '.$v['description']);
+                    console_info('　　　　当前处理：' . $v['name'] . ' ' . $v['description']);
                     $config = AdminConfig::firstOrCreate(['name' => $v['name']], $v);
                     $config->update(['sort' => $key + 1, 'description' => $v['description']]);
                 }
-                console_comment('　　　　处理完成'.PHP_EOL);
+                console_comment('　　　　处理完成' . PHP_EOL);
+                break;
+            case 'delete':
+                console_info('后台删除配置开始处理');
+                $configIds = [];
+                foreach ($adminConfigData as $key => $v) {
+                    $config = AdminConfig::where('name', $v['name'])->first();
+                    if ($config) {
+                        $configIds[] = $config->id;
+                    }
+                }
+                $configs = AdminConfig::whereNotIn('id', $configIds)->get();
+                $count = $configs->count();
+                if ($count == 0) {
+                    console_comment('　　　　处理完成：无将要删除的配置' . PHP_EOL);
+                } else {
+                    foreach ($configs as $config) {
+                        console_info('　　正在删除：' . $config->id . ' ' . $config->name . ' [' . $config->description . ']' . ' ，配置值为：' . $config->value);
+                        $config->delete();
+                    }
+                    console_comment('　　　　处理完成：共删除配置' . $count . '个' . PHP_EOL);
+                }
                 break;
             default:
-                console_error('只允许type参数类型为“new”');
+                console_error('只允许type参数类型为“new”、“delete”');
                 break;
         }
     }
