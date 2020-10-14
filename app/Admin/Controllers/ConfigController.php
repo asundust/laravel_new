0f5e5499cf19bbf8fe24178ab5d27db57f872471
asundust <asundust@qq.com>
2020-10-14 11:14:29 +0800

    修改Admin Config值允许为空

--- app/Admin/Controllers/ConfigController.php
+++ app/Admin/Controllers/ConfigController.php
@@ -104,7 +104,7 @@ class ConfigController extends BaseConfigController
         $form = new Form(new AdminConfig());
 
         $form->display('id', 'ID');
-        $form->display('name', '名称')->rules('required');
+        $form->display('name', '名称');
         $form->textarea('value', '值');
         $form->display('description', '描述');
         $form->display('sort', '排序')->default(0);
