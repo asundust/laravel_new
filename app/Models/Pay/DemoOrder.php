f31b33c69e883dc743a2a5266b22241ad6a71709
asundust <asundust@qq.com>
2021-02-03 14:02:26 +0800

    调整Model

--- app/Models/Pay/DemoOrder.php
+++ app/Models/Pay/DemoOrder.php
@@ -5,7 +5,6 @@ namespace App\Models\Pay;
 use App\Models\BaseModel;
 use App\Models\BaseModelTrait;
 use Eloquent;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
