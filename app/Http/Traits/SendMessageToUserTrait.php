8edf5d7e2898f634ac4aa6e25848596e729cafb8
asundust <asundust@users.noreply.github.com>
2021-07-03 14:51:02 +0000

    Apply fixes from StyleCI

--- app/Http/Traits/SendMessageToUserTrait.php
+++ app/Http/Traits/SendMessageToUserTrait.php
@@ -9,7 +9,7 @@ trait SendMessageToUserTrait
     use SendMessageTrait;
 
     /**
-     * 发送消息
+     * 发送消息.
      *
      * @throws \EasyWeChat\Kernel\Exceptions\InvalidArgumentException
      * @throws \EasyWeChat\Kernel\Exceptions\RuntimeException
@@ -24,7 +24,7 @@ trait SendMessageToUserTrait
                 return sct_send($text, $desc);
                 break;
             case 3:
-                return $this->defaultSend(cache_config('wechat_work_push_user','@all'), $text, $desc);
+                return $this->defaultSend(cache_config('wechat_work_push_user', '@all'), $text, $desc);
                 break;
         }
     }
